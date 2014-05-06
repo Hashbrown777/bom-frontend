ef43489c371ef137f97b9cccf7051fc25f5355c6
bbrcan <s3334276@student.rmit.edu.au>
2014-05-06 03:32:36 +0000

    set root page to redirect to climateanalyser app

--- bom/urls.py
+++ bom/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bom.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    #url(r'^climateanalyser/', include('climateanalyser.urls')),
+    url(r'^$', include('climateanalyser.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
