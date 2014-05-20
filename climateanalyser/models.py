c44f1460589e894a5a34a4716d09487b464bbb75
ben <ben@bom-sand2.(none)>
2014-05-20 05:07:10 +0000

    Code refactoring.

--- climateanalyser/models.py
+++ climateanalyser/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-#from auth.models import ClimateAnalyserUser
 from django.contrib.auth.models import User
 
 class Computation(models.Model):
@@ -7,18 +6,24 @@ class Computation(models.Model):
       created_date = models.DateTimeField('date created')
       completed_date = models.DateTimeField('date completed',null=True)
 
+      #Return all the data files associated with this computation.
       def get_data_files(self):
-         return DataFile.objects.filter(computation=self.id)
+         return DataFile.objects.filter(computation=self.id).order_by('id')
 
+      #Get the URL of the result from Zoo.
       def get_result(self):
          return ZooAdapter.get_result(self.get_data_files())
-
+    
 class ZooAdapter():
 
+   #Get result URL from Zoo.
    @staticmethod
    def get_result(data_files):
 
-      result_path = 'http://130.56.248.143/cgi-bin/operators/zoo_loader.cgi?request=Execute&service=WPS&version=1.0.0.0&identifier=Correlate&DataInputs=url&url1=' + data_files[0].path + '&url2=' + data_files[1].path + '&Fsample.nc'
+      result_path = ('http://130.56.248.143/cgi-bin/operators/zoo_loader.cgi?' 
+                     'request=Execute&service=WPS&version=1.0.0.0&identifier='
+                     'Correlate&DataInputs=url1=' + data_files[0].path + 
+                     ';url2=' + data_files[1].path + '&Fsample.nc')
 
       return result_path
 
--- climateanalyser/views.py
+++ climateanalyser/views.py
@@ -8,43 +8,50 @@ from django.http import HttpResponseRedirect
 from climateanalyser.forms import ComputeForm
 from climateanalyser.models import *
 
-
+#Default page
 def index(request):
    t = loader.get_template('index.html')
    context = RequestContext(request, {})
    html = t.render(context)
    return HttpResponse(html)
 
+#Form for creating new computation
 def compute(request):
 
    if request.method == 'POST':
 
-         form = ComputeForm(request.POST)
+      #grab form with the user input
+      form = ComputeForm(request.POST)
 
-         if form.is_valid():
+      if form.is_valid():
+         
+         #Save our data!
+         admin_user = User.objects.get(username__exact='admin')
 
-            admin_user = User.objects.get(username__exact='admin')
+         #TODO - remove harcoded 'admin' value
+         computation = Computation(created_by=admin_user,
+               created_date=datetime.now())
+         computation.save()
 
-            #TODO - remove harcoded 'admin' value
-            computation = Computation(created_by=admin_user, created_date=datetime.now())
-            computation.save()
+         data_file_1 = DataFile(path=form.cleaned_data['data_file_1'],
+               computation=computation)
+         data_file_2 = DataFile(path=form.cleaned_data['data_file_2'],
+               computation=computation)
 
-            data_file_1 = DataFile(path=form.cleaned_data['data_file_1'],computation=computation)
-            data_file_2 = DataFile(path=form.cleaned_data['data_file_2'],computation=computation)
+         data_file_1.save()
+         data_file_2.save()
 
-            data_file_1.save()
-            data_file_2.save()
-
-            return HttpResponseRedirect('/result?computation=' + str(computation.id))
+         #show result page
+         return HttpResponseRedirect('/result?computation=' 
+               + str(computation.id))
 
    else:
       form = ComputeForm()
 
    return render(request, 'compute_form.html', { 'form' : form, })
 
-
+#Display result after submitting computation
 def result(request):
-
+   #grab computation id from URL string
    computation = Computation.objects.get(id=request.GET.get('computation'))
    return render(request, 'result.html', {'computation': computation})
-
