116d10c5a210a48cb5ab3eb8fc2cc5a0e58f311c
bbrcan <s3334276@student.rmit.edu.au>
2014-05-27 05:03:20 +0000

    Updated result URL for ZooAdapter. Also added calculation field.

--- climateanalyser/models.py
+++ climateanalyser/models.py
@@ -5,6 +5,7 @@ class Computation(models.Model):
       created_by = models.ForeignKey(User)
       created_date = models.DateTimeField('date created')
       completed_date = models.DateTimeField('date completed',null=True)
+      calculation = models.CharField(max_length=100)
 
       #Return all the data files associated with this computation.
       def get_data_files(self):
@@ -12,18 +13,18 @@ class Computation(models.Model):
 
       #Get the URL of the result from Zoo.
       def get_result(self):
-         return ZooAdapter.get_result(self.get_data_files())
+         return ZooAdapter.get_result(self.get_data_files(), self.calculation)
     
 class ZooAdapter():
 
    #Get result URL from Zoo.
    @staticmethod
-   def get_result(data_files):
+   def get_result(data_files, calculation):
 
-      result_path = ('http://130.56.248.143/cgi-bin/operators/zoo_loader.cgi?' 
-                     'request=Execute&service=WPS&version=1.0.0.0&identifier='
-                     'Correlate&DataInputs=url1=' + data_files[0].path + 
-                     ';url2=' + data_files[1].path + '&Fsample.nc')
+      result_path = ('http://130.56.248.143/cgi-bin/zoo_loader.cgi?request='
+                     'Execute&service=WPS&version=1.0.0.0&identifier='
+                     'Operation&DataInputs=selection=' + calculation + ';urls='
+                     + data_files[0].path + ',' + data_files[1].path)
 
       return result_path
 
