fcc35b066938c3d4442a234b1587905c1d547024
bbrcan <s3334276@student.rmit.edu.au>
2014-06-03 04:00:25 +0000

    Implemented backend view for Climateanalyser app.

--- climateanalyser/admin.py
+++ climateanalyser/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from climateanalyser.models import Computation, DataFile
 
-# Register your models here.
+class DataFileInline(admin.StackedInline):
+   model = DataFile
+   extra = 2
+
+class ComputationAdmin(admin.ModelAdmin):
+   list_display = ['id', 'created_by', 'created_date', 'completed_date']
+   inlines = [DataFileInline]
+
+admin.site.register(Computation, ComputationAdmin)
+
+
