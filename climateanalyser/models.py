46a315e7d5dc1ef49610a3fd1e8c6d82bbbfca53
bbrcan <s3334276@student.rmit.edu.au>
2014-07-22 09:31:47 +0000

    Fixed datafile ordering issue.

--- climateanalyser/models.py
+++ climateanalyser/models.py
@@ -5,12 +5,15 @@ import hashlib
 import urllib
 
 class DataFile(models.Model):
+   class Meta:
+      #order by relationship with computation
+      ordering = ['computationdatafile__id']
+
    file_url = models.CharField(max_length=1000)
    cached_file = models.CharField(max_length=1000)
    last_modified = models.DateTimeField('last modified')
 
    def save(self, *args, **kwargs):
-
       #Create cache file
       self.cached_file = hashlib.md5(self.file_url).hexdigest()
       urllib.urlretrieve(self.file_url, 
@@ -33,7 +36,7 @@ class Computation(models.Model):
       calculation = models.CharField(max_length=100,
             choices=CALCULATION_CHOICES, default='correlate')
 
-      datafiles = models.ManyToManyField(DataFile)
+      datafiles = models.ManyToManyField(DataFile,through='ComputationDataFile')
 
       #Get the URL of the result from Zoo.
       def result(self):
@@ -44,7 +47,10 @@ class Computation(models.Model):
             self.created_date = datetime.now()
          super(Computation, self).save(*args, **kwargs)
 
-
+class ComputationDataFile(models.Model):
+   # This class used so we can have separate datafile order per computation
+   datafile = models.ForeignKey(DataFile)
+   computation = models.ForeignKey(Computation)
 
 class ZooAdapter():
 
