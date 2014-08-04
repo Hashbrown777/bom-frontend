22f44ae44224535537df9e5db2db86e43023842c
Dylan McTaggart <s3333915@student.rmit.edu.au>
2014-08-04 02:03:43 +0000

    Finishing off user profile functionality and basic template

--- auth/urls.py
+++ auth/urls.py
@@ -5,7 +5,7 @@ from auth import views
 urlpatterns = [
    url(r'^$', views.index, name='index'),
    url(r'^login/$', 'django.contrib.auth.views.login', { 'template_name' : 'login.html' }),
-   url(r'^logout/$', 'django.contrib.auth.views.logout', { 'template_name' : 'auth/logout.html' }),
+   url(r'^logout/$', 'django.contrib.auth.views.logout', { 'template_name' : 'logout.html' }),
    url(r'^register/$', views.register, name='register'),
    url(r'^user/password/reset/$', 
          'django.contrib.auth.views.password_reset', 
@@ -18,4 +18,5 @@ urlpatterns = [
     {'post_reset_redirect' : '/user/password/done/'}),
    url(r'^user/password/done/$', 
     'django.contrib.auth.views.password_reset_complete'),
+   url(r'^profile/', views.profile, name='profile'),
    ]
