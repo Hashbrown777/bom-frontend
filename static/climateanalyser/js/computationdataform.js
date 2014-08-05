4e8f8c9dc92c963ca2e2044c15e3f4751fa4fe24
bbrcan <s3334276@student.rmit.edu.au>
2014-08-05 02:58:32 +0000

    refactoring js for new computation form

--- static/climateanalyser/js/computationdataform.js
+++ static/climateanalyser/js/computationdataform.js
@@ -4,16 +4,21 @@
 
       var $form = $("form");
 
-      $form.find("select:not([multiple])").change(function(){
+      /**
+       * Populate the variables multi-selectbox based on the selected datafile.
+       *
+       * Will make an ajax call to get list of variables to use.
+       */
+      function populateVariables(datafileSelect){
 
          // ignore "-----" option
-         if (!$(this).val()){
+         if (!$(datafileSelect).val()){
             return;
          }
 
          // get multi-selectbox for variables
-         var $variablesSelect = $("#" + $(this).attr("id").replace("datafile",
-               "variables"));
+         var $variablesSelect = $("#" + $(datafileSelect).attr("id")
+               .replace("datafile", "variables"));
          
          $variablesSelect.html("");
 
@@ -23,7 +28,7 @@
             dataType : "json",
             url : "/load_datafile_metadata",
             data : { 
-               id : $(this).val(),
+               id : $(datafileSelect).val(),
                csrfmiddlewaretoken : $.cookie("csrftoken")
             }
          }).done(function(response){
@@ -34,8 +39,16 @@
                   + response[i] + "</option>");
             }
          });
+      };
 
+      $form.find("select[id$=datafile]").change(function(){
+         //Load variables each time datafile changes
+         populateVariables(this);
+      }).each(function(){ 
+         //if we need to populate variables selectbox on page load
+         populateVariables(this);
       });
+
 
    });
 
