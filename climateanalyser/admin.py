c2a070154ea7829a7693d5c8db96a88ab3ef958c
bbrcan <s3334276@student.rmit.edu.au>
2014-08-07 09:55:13 +0000

    fixed datafile admin forms

--- climateanalyser/admin.py
+++ climateanalyser/admin.py
@@ -27,16 +27,17 @@ class AdminDataFileForm(DataFileForm):
       css = { 'all' : ('climateanalyser/css/admindatafileform.css',) }
 
 class DataFileAdmin(admin.ModelAdmin):
-   form = AdminDataFileForm
    list_display = ['id','file_url','cached_file','last_modified']
 
    def get_form(self, request, obj=None, **kwargs):
       if obj: # edit form
          # make edit form a read-only 'profile' page
+         self.form = AdminDataFileForm
          self.form.fields = ['file_url', 'last_modified', 'cached_file', 'metadata']
          self.readonly_fields = ('file_url', 'last_modified', 'cached_file',
                'metadata')
       else: # add form
+         self.form = DataFileForm
          self.form.fields = ['file_url']
          self.readonly_fields = []
          # don't hide save buttons on add form
