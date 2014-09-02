448de6813be659bd2bfd7686619f6d795d3319a4
bbrcan <s3334276@student.rmit.edu.au>
2014-09-02 15:36:29 +1000

    Updated ajax view for computation status. it now takes rsa-encrypted strings

--- climateanalyser/ajax.py
+++ climateanalyser/ajax.py
@@ -1,6 +1,7 @@
+import json,urllib,rsa,base64
 from django.http import HttpResponse
 from models import DataFile,Computation
-import json
+from zooadapter.models import ZooAdapter
 
 def load_datafile_metadata(request):
 
@@ -18,9 +19,26 @@ def load_datafile_metadata(request):
    return HttpResponse(response)
  
 def update_computation_status(request):
+   """Update the status of a computation. All input from request should be
+   rsa encoded"""
+
+   private_key = ZooAdapter.config.get_private_key()
 
    response = 'failure' # whether or not we failed to update status
-   status = request.POST.get('status')
+   encrypted_status = request.GET.get('status')
+   encrypted_comp_id = request.GET.get('id')
+
+   # no input
+   if not encrypted_status or not encrypted_comp_id:
+      return HttpResponse(response)
+
+   # prepare strings for decryption 
+   encrypted_status = urllib.unquote(base64.b64decode(encrypted_status))
+   encrypted_comp_id = urllib.unquote(base64.b64decode(encrypted_comp_id))
+
+   # decrypt!
+   status = rsa.decrypt(encrypted_status, private_key)
+   computation_id = rsa.decrypt(encrypted_comp_id, private_key)
 
    try:
       computation = Computation.objects.get(id=request.POST.get('id'))
