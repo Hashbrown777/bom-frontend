34158ac9dba67b907312a2fe6ecacaa054f31bd6
Andrew Dunn <s3332747@student.rmit.edu.au>
2014-09-14 20:15:15 +0000

    Add legend to map
    The map will now display a legend in the upper-right hand corner.

--- static/climateanalyser/js/map.js
+++ static/climateanalyser/js/map.js
@@ -9,6 +9,7 @@
          break;
       }
 
+   // First
    var wmsResource = resultPath.split('?')[0];
    var rangeURL = "/get_data_range?wms_resource="
                   + encodeURIComponent(wmsResource)
@@ -17,14 +18,58 @@
    var smallest = -50;
    var largest = 50;
 
+   var WmsLegend = L.Control.extend({
+      options: {
+        position: 'topright'
+      },
+      onAdd: function (map) {
+         // create the control container with a particular class name
+         var container = L.DomUtil.create('div', 'wms-legend');
+         container.className += " leaflet-bar";
+         // Need to subtract 2 from img height to account for borders.
+         var content = "<div class='content'><img src='" +
+                       wmsResource +
+                          "?REQUEST=GetLegendGraphic" +
+                          "&COLORBARONLY=true" +
+                          "&WIDTH=20&HEIGHT=" + (this.height - 2) + "' />" +
+                          "<table cellpadding='0' cellspacing='0' style='" + 
+                             "height: " + this.height + "px'>" +
+                          "<tr><td style='padding: 0;'>&nbsp;</td>";
+         if (this.high <= 0 || this.low >= 0) {
+            content += "<td style='padding: 0;'>" +
+                       "<span class='value'>" + this.high + "</span>";
+            content += "    <span class='value'>" + this.low + "</span></td>";
+         } else {
+            var posPC = this.high / (this.high - this.low);
+            content += "<td style='height: " + (100 * posPC) + "%; "+
+                       "position: relative; padding: 0;" + "'>" +
+                       "<span class='value'>" + this.high + "</span></td>";
+            content += "</tr>" +
+                          "<tr><td style='padding: 0;'>&nbsp;</td>" +
+                          "<td style='" +
+                          "position: relative; padding: 0;" + "'>" +
+                          "<span class='value'>0</span>" +
+                          "<span class='value' >" + this.low + "</span></td>";
+         }
+         content += "</tr></table></div>";
+         container.innerHTML = content;
+         return container;
+      },
+      initialize: function (height, low, high, options) {
+         this.height = height;
+         this.low = Math.round(low * 10) / 10;
+         this.high = Math.round(high * 10) / 10;
+         L.Util.setOptions(this, options);
+      }
+   });
+
    $.ajax({
      url: rangeURL
    })
    .done(function( data ) {
-      if ( console && console.log ) {
-         smallest = data.min;
-         largest = data.max;
-      }
+      smallest = data.min;
+      largest = data.max;
+
       var map = L.map('map').setView([-37.81, 144.96], 2);
       var layer = L.tileLayer.wms(wmsResource, {
                layers: calculation,
@@ -38,6 +83,7 @@
                maxZoom: 8,
                detectRetina: true
                }).addTo(map);
-      });
+      map.addControl(new WmsLegend(400, smallest, largest));
+   });
    });
 })(jQuery);
