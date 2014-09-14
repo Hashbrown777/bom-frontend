0f86e4dc427a565a574678e9d3b214adadb31151
Andrew Dunn <s3332747@student.rmit.edu.au>
2014-09-14 18:42:55 +0000

    Map now shows full range of values.
    The computation map will now automatically adjust the colour range to
    represent the entire gamut of values in a data file.

--- static/climateanalyser/js/map.js
+++ static/climateanalyser/js/map.js
@@ -1,28 +1,43 @@
 (function($){
-
- $(window).ready(function(){
-
-   switch (calculation) {
+   $(window).ready(function(){
+      switch (calculation) {
       case 'correlate':
          calculation = 'correlation';
          break;
       case 'regress':
          calculation = 'regression';
          break;
-   }
+      }
 
-   var map = L.map('map').setView([-37.81, 144.96], 2);
-   var layer = L.tileLayer.wms(resultPath, {
-            layers: calculation,
-            format: 'image/png',   
-            transparent: true,
-            attribution: "Climate Analysis data"
-            }).addTo(map);
+   var wmsResource = resultPath.split('?')[0];
+   var rangeURL = "/get_data_range?wms_resource="
+                  + encodeURIComponent(wmsResource)
+                  + "&layer=" + encodeURIComponent(calculation);
 
-   L.tileLayer(tilemillServerAddress + '/v2/CountryOutlines/{z}/{x}/{y}.png',{
-            maxZoom: 8,
-            detectRetina: true
-            }).addTo(map);
- });
+   var smallest = -50;
+   var largest = 50;
 
+   $.ajax({
+     url: rangeURL
+   })
+   .done(function( data ) {
+      if ( console && console.log ) {
+         smallest = data.min;
+         largest = data.max;
+      }
+      var map = L.map('map').setView([-37.81, 144.96], 2);
+      var layer = L.tileLayer.wms(wmsResource, {
+               layers: calculation,
+               format: 'image/png',   
+               transparent: true,
+               attribution: "Climate Analyser",
+               COLORSCALERANGE: smallest + "%2C" + largest
+               }).addTo(map);
+
+      L.tileLayer(tilemillServerAddress + '/v2/CountryOutlines/{z}/{x}/{y}.png',{
+               maxZoom: 8,
+               detectRetina: true
+               }).addTo(map);
+      });
+   });
 })(jQuery);
